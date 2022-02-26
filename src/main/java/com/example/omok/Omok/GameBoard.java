51019a08d5100cad66ea4f61979a73e8fa3c86ab
Sooyoung <jeonso3412@gmail.com>
2022-02-26 17:56:53 -0500

    initial calc

---
+++ src/main/java/com/example/omok/Omok/GameBoard.java
@@ -0,0 +1,57 @@
+package com.example.omok.Omok;
+
+public class GameBoard {
+
+    private Stone[][] board;
+    private int size;
+    // Black = false
+    // White = true
+    private boolean playerTurn;
+
+    public GameBoard(int size) {
+        board = new Stone[size][size];
+        this.size = size;
+        playerTurn = true;
+    }
+
+    public Stone fillBoard(int x, int y) {
+        String color = "";
+        if (playerTurn) color = "white";
+        else color = "black";
+        Stone s = new Stone(color, x, y);
+        board[y - 1][x - 1] = s;
+        return s;
+    }
+
+    public boolean isEmpty(int x, int y) {
+        if (board[y - 1][x - 1] == null) return true;
+        return false;
+    }
+
+    public void changeTurn() {
+        playerTurn = !playerTurn;
+    }
+
+    public void displayBoard() {
+        for (Stone[] line : board) {
+            for (Stone s : line) {
+                if (s == null) System.out.print("❔  ");
+                else if (s.getColor().equals("black")) System.out.print("⚪  ");
+                else if (s.getColor().equals("white")) System.out.print("⚫  ");
+            }
+            System.out.println();
+        }
+    }
+
+    public Stone[][] getBoard() {
+        return this.board;
+    }
+
+    public int getSize() {
+        return this.size;
+    }
+
+    public boolean getPlayerTurn() {
+        return playerTurn;
+    }
+}
---
+++ src/main/java/com/example/omok/Omok/Omok.java
@@ -0,0 +1,12 @@
+package com.example.omok.Omok;
+
+public class Omok {
+
+    private static GameBoard board;
+
+    public static void runOmok() {
+        int size = 10;
+        board = new GameBoard(size);
+    }
+
+}
