69348b56d2524505d71ba0cad4170f4f78d8b6cf
Sooyoung <jeonso3412@gmail.com>
2022-02-27 15:17:13 -0500

    good

--- src/main/java/com/example/omok/GUI.java
+++ src/main/java/com/example/omok/GUI.java
@@ -102,8 +102,8 @@ public class GUI implements ActionListener {
                 b.displayBoard();
                 AI.displayResult();
             }
-            
         }
+
     }
 
     public void check(){
